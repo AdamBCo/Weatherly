18da39f749ef3c6b3c74525083ddda3e4e7c4258
Adam <AdamBCo@me.com>
2014-12-22 17:28:13 -0700

    Initial Commit

---
+++ Weatherly/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Weatherly
+//
+//  Created by Adam Cooper on 12/22/14.
+//  Copyright (c) 2014 Adam Cooper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
